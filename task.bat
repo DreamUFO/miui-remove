406198bb6f894976d4c5e3681830542ac8ebc4ce
DreamUFO <396269179@qq.com>
2023-11-20 15:43:31 +0800

    Create task.bat
    初始版本

---
+++ task.bat
@@ -0,0 +1,7 @@
+@echo off
+
+adb shell pm uninstall --user 0 com.miui.translationservice
+adb shell pm uninstall --user 0 com.miui.translation.xmcloud
+adb shell pm uninstall --user 0 com.xiaomi.barrage
+adb shell pm uninstall --user 0 com.xiaomi.aireco
+adb shell pm uninstall --user 0 com.miui.touchassistant
